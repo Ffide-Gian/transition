2255dbd7fe5486626cbeaf74852be71c992826fa
Ffide-Gian <giancaibigan@gmail.com>
2025-02-11 13:52:43 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Example JavaScript code
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Page is fully loaded and JavaScript is working!");
-    
-    const heading = document.querySelector('h1');
-    heading.textContent = "JavaScript is now active on this page!";
-});
