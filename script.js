2dd7a1586df95a937906664644a763e673b922ef
Ffide-Gian <giancaibigan@gmail.com>
2025-02-11 08:49:38 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example JavaScript code
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Page is fully loaded and JavaScript is working!");
+    
+    const heading = document.querySelector('h1');
+    heading.textContent = "JavaScript is now active on this page!";
+});
